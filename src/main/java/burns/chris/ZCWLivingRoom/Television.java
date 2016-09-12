612ca1463feb5bc6f580339e3bb42619c6a85d1e
Christopher Burns <christopherburns@Chris-Burns-ZCW-6.local>
2016-09-12 14:40:07 -0400

    First commit

---
+++ src/main/java/burns/chris/ZCWLivingRoom/Television.java
@@ -0,0 +1,24 @@
+package burns.chris.ZCWLivingRoom;
+
+/**
+ * Created by christopherburns on 9/10/16.
+ */
+public class Television {
+    private Integer volume;
+    private Integer channel;
+
+    public Television() {
+        int volume = 0;
+        int channel = 0;
+    }
+    public Integer getVolume(){
+        return 0;
+    }
+
+    public void setVolume(Integer amount){
+        volume = amount;
+
+
+    }
+
+}
---
+++ src/test/java/burns/chris/ZCWLivingRoom/TelevisionTest.java
@@ -0,0 +1,22 @@
+package burns.chris.ZCWLivingRoom;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TelevisionTest {
+    @Test
+    public void getVolumeTest() {
+        Television television = new Television();
+        Integer expectedValue = 0;
+        Integer actualValue = television.getVolume();
+        assertEquals("The expected value is 0", expectedValue, actualValue);
+    public void setVolumeTest() {
+        Television television = new Television ();
+
+    }
+
+    }
+
+
+    }
+}
